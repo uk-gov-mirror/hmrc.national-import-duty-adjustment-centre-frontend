2c98847ce62fa1f8722262a9fbb5bf9b4f5b46a3
rorticus0502 <rorticus0502@gmail.com>
2021-02-18 13:24:16 +0000

    NF-230 addressed review comments

--- test/uk/gov/hmrc/nationalimportdutyadjustmentcentrefrontend/navigation/NavigatorSpec.scala
+++ test/uk/gov/hmrc/nationalimportdutyadjustmentcentrefrontend/navigation/NavigatorSpec.scala
@@ -40,12 +40,12 @@ class NavigatorSpec extends UnitSpec with Injector with TestData {
     val previousPage = back(AddressPage, _)
 
     "going forward" should {
-      "dont go to importer has EORI page for importer" in {
+      "go to bank details page if claimant is importer" in {
         nextPage(
           answers().copy(representationType = Some(RepresentationType.Importer))
         ) mustBe routes.BankDetailsController.onPageLoad()
       }
-      "do go to importer has EORI page for representative" in {
+      "go to does importer have EORI page if claimant is representative" in {
         nextPage(
           answers().copy(representationType = Some(RepresentationType.Representative))
         ) mustBe routes.ImporterHasEoriController.onPageLoad()
@@ -54,23 +54,6 @@ class NavigatorSpec extends UnitSpec with Injector with TestData {
     "going back" should {
       "go to contact details page" in {
         previousPage(answers()) mustBe routes.ContactDetailsController.onPageLoad()
-      }
-    }
-  }
-
-  "Navigator from importer eori page" when {
-    val nextPage     = navigator.nextPage(ImporterHasEoriPage, _)
-    val previousPage = back(ImporterHasEoriPage, _)
-
-    "going forward" should {
-      "go to repay page" in {
-        nextPage(answers().copy(importerHasEori = Some(true))) mustBe routes.RepayToController.onPageLoad()
-        nextPage(answers().copy(importerHasEori = Some(false))) mustBe routes.RepayToController.onPageLoad()
-      }
-    }
-    "going back" should {
-      "go to address details page" in {
-        previousPage(answers()) mustBe routes.AddressController.onPageLoad()
       }
     }
   }
