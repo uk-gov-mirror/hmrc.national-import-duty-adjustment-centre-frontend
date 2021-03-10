0a50c76b174b4ff71659edf9d987e284592a5b4a
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2021-03-10 12:13:49 +0000

    BAU - use prevent double click flag on GovukButton (#109)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-27" % "3.2.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "0.34.0-play-27",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "0.50.0-play-27",
     "uk.gov.hmrc"       %% "play-frontend-govuk"        % "0.56.0-play-27",
     "uk.gov.hmrc"       %% "play-language"              % "4.5.0-play-27",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-27"         % "0.47.0"
