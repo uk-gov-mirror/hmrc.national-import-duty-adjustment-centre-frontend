d74b831a22dfd77765a8e3f655f2fff058503dbb
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2021-03-24 11:41:11 +0000

    BAU - update library dependencies (#141)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,15 +4,15 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27" % "3.2.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "0.50.0-play-27",
-    "uk.gov.hmrc"       %% "play-frontend-govuk"        % "0.56.0-play-27",
-    "uk.gov.hmrc"       %% "play-language"              % "4.5.0-play-27",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-27" % "4.1.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "0.54.0-play-27",
+    "uk.gov.hmrc"       %% "play-frontend-govuk"        % "0.67.0-play-27",
+    "uk.gov.hmrc"       %% "play-language"              % "4.12.0-play-27",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-27"         % "0.47.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-27" % "3.2.0"   % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-27" % "4.1.0"   % Test,
     "org.scalatest"          %% "scalatest"              % "3.2.3"   % Test,
     "org.scalatestplus"      %% "mockito-3-4"            % "3.2.3.0" % Test,
     "org.jsoup"               % "jsoup"                  % "1.13.1"  % Test,
