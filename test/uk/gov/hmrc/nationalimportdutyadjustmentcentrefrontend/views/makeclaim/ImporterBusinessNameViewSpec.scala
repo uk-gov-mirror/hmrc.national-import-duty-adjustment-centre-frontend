8bf00c41ed6f0053d08abccca657e3b85f833e2e
rorticus0502 <rorticus0502@gmail.com>
2021-04-14 11:25:29 +0100

    added Form Provider tests

---
+++ test/uk/gov/hmrc/nationalimportdutyadjustmentcentrefrontend/views/makeclaim/ImporterBusinessNameViewSpec.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.nationalimportdutyadjustmentcentrefrontend.views.makeclaim
+
+import org.jsoup.nodes.Document
+import play.api.data.Form
+import uk.gov.hmrc.nationalimportdutyadjustmentcentrefrontend.base.{TestData, UnitViewSpec}
+import uk.gov.hmrc.nationalimportdutyadjustmentcentrefrontend.forms.create.ImporterBusinessNameFormProvider
+import uk.gov.hmrc.nationalimportdutyadjustmentcentrefrontend.models.create.ImporterBusinessName
+import uk.gov.hmrc.nationalimportdutyadjustmentcentrefrontend.views.html.makeclaim.ImporterBusinessNameView
+
+class ImporterBusinessNameViewSpec extends UnitViewSpec with TestData {
+
+  private val page = instanceOf[ImporterBusinessNameView]
+  private val form = new ImporterBusinessNameFormProvider().apply()
+
+  private def view(form: Form[ImporterBusinessName] = form): Document = page(form, navigatorBack)
+
+  "Business Name page on empty form" should {
+
+    "have correct title" in {
+      view().title() must startWith(messages("importer.businessName.title"))
+    }
+
+    "have correct heading" in {
+      view().getElementsByTag("h1") must containMessage("importer.businessName.title")
+    }
+
+    "have back link" in {
+      view() must haveNavigatorBackLink(navigatorBackUrl)
+    }
+
+    "have 'Continue' button" in {
+      view().getElementById("nidac-continue") must includeMessage("site.continue")
+    }
+
+  }
+
+  "AddressPage on filled form" should {
+
+    "have populated fields" in {
+      val filledView = view(form.fill(importerBusinessNameAnswer))
+
+      filledView.getElementById("name") must haveValue(importerBusinessNameAnswer.name)
+    }
+
+    "display error when " when {
+
+      "importers business name is missing" in {
+        val errorView = view(form.bind(Map("name" -> "")))
+        errorView must haveFieldError("name", "importer.businessName.name.error.required")
+        errorView must havePageError("importer.businessName.name.error.required")
+      }
+
+    }
+
+  }
+}
