dc6db57ef9054d8b59323cb518e8df9f5acd84b5
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2021-04-15 16:55:48 +0100

    NF-403 and NF-404 - file upload improvements (#163)

--- app/assets/javascripts/nidacfrontend.js
+++ app/assets/javascripts/nidacfrontend.js
@@ -10,4 +10,15 @@ if (window.history && window.history.replaceState && typeof window.history.repla
   window.history.replaceState(null, null, window.location.href);
 }
 
-
+// automatically submit document upload on file select
+function onFileSelect() {
+  try{
+    // NF-403 - hide the form controls, display the spinner and disable the 'Continue' button before submitting
+    document.getElementById("upload-form").classList.add("govuk-!-display-none");
+    document.querySelector(".hidden-spinner-row").classList.remove("govuk-!-display-none");
+    document.getElementById("nidac-continue").disabled = true;
+  }
+  finally {
+    document.getElementById("upload-form").submit();
+  }
+}
